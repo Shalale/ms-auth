6dc28443c6d0f0d1240242fae804b1e8cf5a6c49
Shalale <selale.memmedli11@gmail.com>
2022-11-26 11:08:01 +0400

    redis db added

--- src/main/java/com/example/msauth/controller/AuthController.java
+++ src/main/java/com/example/msauth/controller/AuthController.java
@@ -1,6 +1,11 @@
 package com.example.msauth.controller;
 
+import com.example.msauth.model.dto.SignInDto;
+import com.example.msauth.model.dto.TokenDto;
+import com.example.msauth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("api/tokens")
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/sign-in")
+    public TokenDto signIn(@RequestBody SignInDto dto){
+        return authService.signIn(dto);
+    }
 }
